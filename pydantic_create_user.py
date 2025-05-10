16c7dad28e05ea5ec5a04ea1911ff41d62991ec5
Ruslan Araslanov <araslanov.r.v@yandex.ru>
2025-05-10 22:02:34 +0500

    Знакомство с Pydantic. Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -3,6 +3,9 @@ from typing import Annotated
 
 
 class ShortUserSchema(BaseModel):
+    """
+    Описание базовой структуры пользователя.
+    """
     email: EmailStr
     last_name: str = Field(alias='lastName')
     first_name: str = Field(alias='firstName')
@@ -10,12 +13,18 @@ class ShortUserSchema(BaseModel):
 
 
 class UserSchema(ShortUserSchema):
+    """
+    Описание структуры пользователя с полем id.
+    """
     id: UUID4
 
 
 # constr function is discouraged in favor of using Annotated with StringConstraints instead.
 # constr function will be deprecated in Pydantic 3.0.
 class CreateUserRequestSchema(ShortUserSchema):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
     password: Annotated[
         str,
         StringConstraints(pattern=r'^\w{8,25}$')
@@ -23,4 +32,7 @@ class CreateUserRequestSchema(ShortUserSchema):
 
 
 class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
     user: UserSchema
