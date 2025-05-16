980e0767f3cb02130afdc9e316157ac2589d3fa8
Ruslan Araslanov <araslanov.r.v@yandex.ru>
2025-05-16 22:34:54 +0500

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,10 +1,11 @@
 from http import HTTPStatus
 
-from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.private_users_client import get_private_users_client
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -13,22 +14,18 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    # Инициализация клиентов
-    public_users_client = get_public_users_client()
-    authentication_client = get_authentication_client()
+def test_login(
+        function_user: UserFixture,
+        authentication_client: AuthenticationClient
+):
+    # Запрос на логин (login_request -> request)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    # Выполняем логин (login_response -> response)
+    response = authentication_client.login_api(request)
+    # Валидация ответа (login_response_data -> response_data)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    # Создание нового пользователя
-    request = CreateUserRequestSchema()
-    response = public_users_client.create_user_api(request)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    # Аутентификация созданного пользователя
-    login_request = LoginRequestSchema(email=request.email, password=request.password)
-    login_response = authentication_client.login_api(login_request)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-
-    # Проверка статус-кода, проверка корректности тела ответа и валидация JSON-схемы
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
-
+    validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,5 +1,6 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from tools.assertions.base import assert_equal
+from clients.users.users_schema import UserSchema
 
 
 def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
@@ -14,3 +15,29 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Проверяет, что фактические данные пользователя соответствуют ожидаемым.
+
+    :param actual: Исходный ответ на запрос на создание пользователя.
+    :param expected: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+def assert_get_user_response(get_user_response : GetUserResponseSchema, create_user_response :CreateUserResponseSchema):
+    """
+    Проверяет, что ответ на получение данных пользователя соответствует данным созданного пользователя.
+
+    :param get_user_response: Ответ на запрос на получение данных пользователя.
+    :param create_user_response: Ответ API с данными созданного пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_user(create_user_response.user, get_user_response.user)
